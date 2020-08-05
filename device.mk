44e7f8d97d3a590d3292b77bf413cdf4df7b0a35
bhi768 <bhimanand7@gmail.com>
2020-08-05 03:03:40 +0000

    fix the derp

--- device.mk
+++ device.mk
@@ -32,7 +32,7 @@ $(call inherit-product, vendor/dirac/dirac.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-system
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
